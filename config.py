6c7babdbe76421e31880cb3bab2fba95a0be3a9d
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2025-03-09 06:30:48 +0000

    add bigquery to streamlit

--- config.py
+++ config.py
@@ -12,6 +12,15 @@ FIREBASE_MESSAGING_SENDER_ID = os.getenv("FIREBASE_MESSAGING_SENDER_ID")
 FIREBASE_APP_ID = os.getenv("FIREBASE_APP_ID")
 FIREBASE_MEASUREMENT_ID = os.getenv("FIREBASE_MEASUREMENT_ID")
 
+PROJECT_ID = os.getenv("PROJECT_ID")
+PRIVATE_KEY_ID = os.getenv("PRIVATE_KEY_ID")
+PRIVATE_KEY = os.getenv("PRIVATE_KEY").replace("\\n", "\n")
+CLIENT_EMAIL = os.getenv("CLIENT_EMAIL")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_X509_CERT_URL = os.getenv("CLIENT_X509_CERT_URL")
+
+BIGQUERY_DATASET_ID = os.getenv("BIGQUERY_DATASET_ID")
+BIGQUERY_TABLE_ID = os.getenv("BIGQUERY_TABLE_ID")
 
 firebase = {
     "apiKey": FIREBASE_API_KEY,
@@ -22,4 +31,19 @@ firebase = {
     "appId": FIREBASE_APP_ID,
     "measurementId": FIREBASE_MEASUREMENT_ID,
     "databaseURL": "",
+}
+
+
+credentials_dict = {
+    "type": "service_account",
+    "project_id": PROJECT_ID,
+    "private_key_id": PRIVATE_KEY_ID,
+    "private_key": PRIVATE_KEY,
+    "client_email": CLIENT_EMAIL,
+    "client_id": CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": CLIENT_X509_CERT_URL,
+    "universe_domain": "googleapis.com"
 }
